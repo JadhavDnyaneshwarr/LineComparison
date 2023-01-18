76b5302d499e5aa3373360d3d7cd93d14b560db0
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-18 17:38:43 +0530

    Welcome to line comparison computation program

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("welcome to Line Comparison computation program");
+    }
+}
